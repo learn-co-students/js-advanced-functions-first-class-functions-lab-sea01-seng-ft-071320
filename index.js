ba95f26b25f534a6ab408b7e0b00ae52462558db
cortezd334 <cortezd334@gmail.com>
2020-09-02 21:13:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const returnFirstTwoDrivers = (array) {
-    return array[0, 1]
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(array.length-2, array.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function(fare){
+        return fare*integer
+    }
+}
+
+const fareDoubler = function(fare) {
+    return 2 * createFareMultiplier(1)(fare)
+}
+
+const fareTripler = function(fare) {
+    return 3 * createFareMultiplier(1)(fare)
+}
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers)
 }
